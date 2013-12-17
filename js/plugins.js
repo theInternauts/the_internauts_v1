35ce270a616e8f292d0ac8b3e25a1f4b1e02668a
Christopher "Cricket" Wallace <cricketw@wearetheinternauts.com>
2013-12-16 23:48:07 -0800

    updated Fader_Block plugin

--- js/plugins.js
+++ js/plugins.js
@@ -21,23 +21,43 @@
     }
 }());
 
-;(function(){
+
+;(function( $, window, document, undefined ){
     $.fn.fader_block = function( options ){
         var settings = $.extend( true, {}, $.fn.fader_block.defaults, options )
+
+        window.setInterval(function(){fader_cycle(settings)},settings.interval)
         
-        var list = $('#qualities_cloud li')
-        var count = list.length-1
-        console.log("count", count)
-        var randomNumber = Math.floor(Math.random()*10)
-        list[randomNumber].setAttribute('class', settings.initialAction)
         return this
     }// end $.fn.fader_block()
 
+    function buildNumArray( number, upperBound ){
+        var numArray = new Array(number)
+        for(var i =0; i < numArray.length; i++){
+            numArray[i] = Math.floor(Math.random()*upperBound)
+        }
+        return numArray
+    }
+
+    function fader_cycle( settings ){
+        var list = $(settings.selector)
+
+        list.addClass(settings.revertAction)
+        list.removeClass(settings.initialAction)
+        selectiedPositions = buildNumArray(settings.selectionCount, list.length)
+        for(var i = 0; i < selectiedPositions.length; i++){
+            list[selectiedPositions[i]].setAttribute('class', settings.initialAction)
+        }
+    }
 
     $.fn.fader_block.defaults = {
         initialAction: 'light-cloud',
-        revertAction: 'dark-cloud'
+        revertAction: 'dark-cloud',
+        selector: '#qualities_cloud li',
+        selectionCount: 4,
+        interval: 3000
     }
-}())
+}( jQuery, window, document ))
+
 
 // Place any jQuery/helper plugins in here.
